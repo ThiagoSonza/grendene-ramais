6f8666182fc24e13cad1cbef07ec8fd21e4ced59
Thiago Sonza <thiagosonza@MacBook-Air-de-Thiago.local>
2022-01-20 20:10:03 -0300

    Initial Commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import './style.css'
+
+function Footer() {
+
+    return (
+        <div className="footer">
+            <span> Desenvolvido por Grendene :: Fabrica de Softwares - 2022 :: Versão 1.0.1014.5</span>
+        </div>
+    )
+
+}
+
+export default Footer
---
+++ src/components/Menu/index.js
@@ -0,0 +1,26 @@
+import './style.css'
+
+function Menu() {
+
+    return (
+        <>
+            <div className="menu">
+
+                <span> Sistema Interno Sistema </span>
+
+            </div>
+
+            <div className="submenu">
+                <div className="item-submenu"> Item A </div>
+                <div className="item-submenu"> Item B </div>
+                <div className="item-submenu"> Item C </div>
+                <div className="item-submenu"> Item C </div>
+                <div className="item-submenu"> Item D </div>
+                
+            </div>
+        </>
+    )
+
+}
+
+export default Menu
