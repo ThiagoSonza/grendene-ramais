c778a6061d0c1c095d7faa9dcd0980706b2e02cd
Thiago Sonza <thiagosonza@MacBook-Air-de-Thiago.local>
2022-01-20 22:20:13 -0300

    Ajustes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,6 +1,18 @@
+import { useEffect, useRef, useState } from 'react'
 import './style.css'
 
 function Menu() {
+
+    const [menu, setMenu] = useState()
+
+    function handleShowItens(submenu) {
+        setMenu(submenu)
+    }
+
+    function mouseOut(e) {
+        if (menu)
+            setMenu('')
+    }
 
     return (
         <>
@@ -10,13 +22,27 @@ function Menu() {
 
             </div>
 
-            <div className="submenu">
-                <div className="item-submenu"> Item A </div>
-                <div className="item-submenu"> Item B </div>
-                <div className="item-submenu"> Item C </div>
-                <div className="item-submenu"> Item C </div>
-                <div className="item-submenu"> Item D </div>
-                
+            <div className="submenu" onMouseLeave={mouseOut}>
+                <div id="cadastros" className="item-submenu" onMouseOver={() => handleShowItens('cadastros')}>
+                    <span>Cadastros</span>
+                    {menu === 'cadastros' &&
+                        <div onMouseLeave={mouseOut}>
+                            <div className="item"> Padrões </div>
+                            <div className="item"> Usuários </div>
+                            <div className="item"> Perfis </div>
+                        </div>
+                    }
+                </div>
+                <div id="consultas" className="item-submenu" onMouseOver={() => handleShowItens('consultas')}> 
+                    <span>Consultas</span>
+                    {menu === 'consultas' &&
+                        <div onMouseLeave={mouseOut}>
+                            <div className="item"> aa </div>
+                            <div className="item"> bb </div>
+                            <div className="item"> cc </div>
+                        </div>
+                    }
+                </div>
             </div>
         </>
     )
